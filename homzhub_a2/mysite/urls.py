73d6f18b20f4714eaea5b7b2eb91597b40288b29
Chandu sanjith <chandusanjith.t.2610@gmail.com>
2020-08-13 07:08:23 +0000

    static files issue part3

--- homzhub_a2/mysite/urls.py
+++ homzhub_a2/mysite/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from .views import RequestView, Logout, Login, Signup, AddRequest, ReqDetail, OpenAssignment2
@@ -38,3 +39,7 @@ urlpatterns = [
     path('OpenAssignment2/', OpenAssignment2, name='OpenAssignment2'),
     path('ReqDetail/<id>/', ReqDetail, name='ReqDetail'),
 ]
+
+urlpatterns = [
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
